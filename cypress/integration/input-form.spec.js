20c2ce1f3c1c2606ca40a49ed56bbf203e62e147
Annie Huang <annie.hn@gmail.com>
2019-01-21 20:53:16 +1100

    Initial commit;

---
+++ cypress/integration/input-form.spec.js
@@ -0,0 +1,64 @@
+describe('Input form', () => {
+    beforeEach(() => {
+        // cy.visit('/');
+        cy.seedAndVisit([]);
+    });
+
+    it('focuses input on load', () => {
+        // focus whatever element that is available in our class.
+        cy.focused()
+            .should('have.class', 'new-todo')
+    });
+
+    it('accepts input', () => {
+        const typedText = 'Buy Milk';
+
+        cy.get('.new-todo')
+            .type(typedText)
+            .should('have.value', typedText)
+    });
+
+    context('Form submission', () => {
+        beforeEach(() => {
+            cy.server();
+        });
+
+        it('Adds a new todo on submit', () => {
+            const itemText = 'Buy eggs';
+            cy.route('POST', '/api/todos', {
+                name: itemText,
+                id: 1,
+                isComplete: false
+            });
+
+            // cy.pause();
+
+            cy.get('.new-todo')
+                .type(itemText)
+                .type('{enter}')
+                .should('have.value', '');
+            cy.get('.todo-list li')
+                .should('have.length', 1)
+                .and('contain', itemText);
+        });
+
+        it('Shows an error message on a failed submission', () => {
+            cy.route({
+                url: '/api/todos',
+                method: 'POST',
+                status: 500,
+                response: {}
+            });
+
+            // The following means type the word 'test', and then click enter.
+            cy.get('.new-todo')
+                .type('test{enter}');
+
+            cy.get('.todo-list li')
+                .should('not.exist');
+
+            cy.get('.error')
+                .should('be.visible');
+        })
+    })
+});
---
+++ cypress/integration/list-items.spec.js
@@ -0,0 +1,73 @@
+describe('List items', () => {
+    beforeEach(() => {
+        cy.seedAndVisit();
+    });
+
+    it('properly displays completed items', () => {
+        cy.get('.todo-list li')
+            .filter('.completed')
+            .should('have.length', 1)
+            .and('contain', 'Eggs')
+            .find('.toggle')
+            .should('be.checked');
+    });
+
+    it('Shows remaining todos in the footer', () => {
+        cy.get('.todo-count')
+            .filter('.completed')
+            .and('contain', 3);
+    });
+
+    it('Removes a todo', () => {
+        cy.route({
+            url: '/api/todos/1',
+            method: 'DELETE',
+            status: 200,
+            response: {}
+        });
+
+        // .click({force: true});
+        cy.get('.todo-list li')
+            .as('list');
+
+        cy.get('@list')
+            .first()
+            .find('.destroy')
+            .invoke('show')
+            .click();
+
+        cy.get('@list')
+            .should('have.length', 3)
+            .and('not.contain', 'Milk')
+    });
+
+    it('Marks an incomplete item complete', () => {
+        cy.fixture('todos')
+            .then(todos => {
+                // the '_' is the loadash function
+                // Important: remember to use `` when you deal with ${target.id}, otherwise it will not work and it's hard to find out why...
+                const target = Cypress._.head(todos);
+                // console.log('target=', target);
+                cy.route(
+                    'PUT',
+                    `/api/todos/${target.id}`,
+                    Cypress._.merge(target, {isComplete: true})
+                );
+            });
+
+        cy.get('.todo-list li')
+            .first()
+            .as('first-todo');
+
+        cy.get('@first-todo')
+            .find('.toggle')
+            .click()
+            .should('be.checked');
+
+        cy.get('@first-todo')
+            .should('have.class', 'completed');
+
+        cy.get('.todo-count')
+            .should('contain', 2);
+    });
+});
